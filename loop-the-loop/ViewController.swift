52db37ac7c3f12f6ae66a888e15befecf62a8439
henteko <henteko07@gmail.com>
2016-03-05 16:53:21 +0900

    init

---
+++ loop-the-loop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  loop-the-loop
+//
+//  Created by kenta.imai on 2016/03/05.
+//  Copyright © 2016年 henteko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
