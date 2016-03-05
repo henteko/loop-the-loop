28d351cf24825381b2d2726d453d50d8da73c5a2
henteko <henteko07@gmail.com>
2016-03-05 17:26:42 +0900

    previewを表示することに成功した

--- loop-the-loop/ViewController.swift
+++ loop-the-loop/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var captureView: UIView!
+    var videoLayer: AVCaptureVideoPreviewLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let captureSession = AVCaptureSession()
+        let videoDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        let fileOutput = AVCaptureMovieFileOutput()
+        
+        let videoInput: AVCaptureInput!
+        do {
+            videoInput = try AVCaptureDeviceInput.init(device: videoDevice)
+        }catch {
+            videoInput = nil
+        }
+        captureSession.addInput(videoInput)
+        captureSession.addOutput(fileOutput)
+        
+        // Preview
+        self.videoLayer = AVCaptureVideoPreviewLayer.init(session: captureSession)
+        self.videoLayer.frame = self.captureView.bounds
+        self.videoLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        self.captureView.layer.addSublayer(videoLayer)
+        
+        captureSession.startRunning()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
